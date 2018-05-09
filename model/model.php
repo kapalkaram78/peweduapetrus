1ca47a26bc5a49ed2aa19e01578e87be8a4b4727
kapalkaram78 <petrus.indra.wijaya@student.untan.ac.id>
2018-05-09 16:31:11 +0700

    edit model

--- model/model.php
+++ model/model.php
@@ -4,16 +4,4 @@ namespace model;
 class Model{
 	public $config;
 }
-class Mahasiswa extends Model{
-	public $nim;
-	public $ipk;
-
-	public function munculkanNama()
-	{
-		echo "Halo saya Sisfo";
-	}
-}
-
-$santi = new Mahasiswa();
-echo $santi->munculkanNama();
  ?>
