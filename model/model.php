09b9532e616f6acc98dc34a3ef26c6cddffaa9bf
kapalkaram78 <petrus.indra.wijaya@student.untan.ac.id>
2018-05-09 15:51:58 +0700

    membuat model mahasiswa

--- model/model.php
+++ model/model.php
@@ -1,8 +1,17 @@
 <?php 
 namespace model;
 
-class model
-{
+class model{
 	public $config;
 }
+class Mahasiswa extends model{
+	public $nim;
+	public $ipk;
+
+	public function munculkan_nama()
+	{
+		echo "Halo saya Sisfo";
+	}
+}
+
  ?>
